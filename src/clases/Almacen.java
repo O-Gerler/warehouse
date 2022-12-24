e76cf6820d4729baf470a66a5dde84ae18eb5a77
Oier <azurmendi5o@outlook.com>
2022-12-24 15:05:35 +0100

    constructor y metodo cargasDatos

--- src/clases/Almacen.java
+++ src/clases/Almacen.java
@@ -1,5 +1,13 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Almacen {
+	private ArrayList<Articulo> articulos = new ArrayList<>();
+
+	public Almacen() {}
 	
+	public void cargarDatos() {
+		//con un fichero
+	}
 }
