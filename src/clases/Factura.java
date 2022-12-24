bc0f0c623ecd5caa75e17822580deecfe638faa5
Oier <azurmendi5o@outlook.com>
2022-12-24 18:18:50 +0100

    atributos setter/getters

--- src/clases/Factura.java
+++ src/clases/Factura.java
@@ -1,5 +1,36 @@
 package clases;
 
-public class Factura {
+import java.util.Date;
 
+public class Factura {
+	private static final int IVA = 21;
+	private int numero;
+	private String nombreEmpresa;
+	private Date fecha;
+	private String concepto;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
 }
