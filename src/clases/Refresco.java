a9ad1130a41e15e347de5ad657b9bf059eaa673b
Oier <azurmendi5o@outlook.com>
2022-12-24 13:16:53 +0100

    atributos y setters/getters

--- src/clases/Refresco.java
+++ src/clases/Refresco.java
@@ -1,6 +1,43 @@
 package clases;
 
 public class Refresco extends Articulo{
+	
+	private String sabor;
+	private boolean zumo;
+	private boolean gaseoso;
+	private int cantidadAzucar;
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+	public boolean isZumo() {
+		return zumo;
+	}
+
+	public void setZumo(boolean zumo) {
+		this.zumo = zumo;
+	}
+
+	public boolean isGaseoso() {
+		return gaseoso;
+	}
+
+	public void setGaseoso(boolean gaseoso) {
+		this.gaseoso = gaseoso;
+	}
+
+	public int getCantidadAzucar() {
+		return cantidadAzucar;
+	}
+
+	public void setCantidadAzucar(int cantidadAzucar) {
+		this.cantidadAzucar = cantidadAzucar;
+	}
 
 	@Override
 	public void visualizarAticulo() {
