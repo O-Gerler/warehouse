4c60f8429a6003aaf526dcb901fca1ce06c752a0
Oier <azurmendi5o@outlook.com>
2022-12-24 14:30:18 +0100

    atributos y setters/getters

--- src/clases/Cerveza.java
+++ src/clases/Cerveza.java
@@ -3,6 +3,34 @@ package clases;
 import interfaces.Alcoholico;
 
 public class Cerveza extends Articulo implements Alcoholico{
+	
+	private String origen;
+	private String cereales;
+	private double gradosAlcohol;
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getCereales() {
+		return cereales;
+	}
+
+	public void setCereales(String cereales) {
+		this.cereales = cereales;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
 
 	@Override
 	public boolean esFuerte() {
