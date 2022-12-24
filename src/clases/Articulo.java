076b58335ebb11cb7a1abbcab22be3581d6de4cb
Oier <azurmendi5o@outlook.com>
2022-12-24 12:45:35 +0100

    atributos y setters/getters

--- src/clases/Articulo.java
+++ src/clases/Articulo.java
@@ -1,5 +1,49 @@
 package clases;
 
 public class Articulo {
-
+	private String name;
+	private String code;
+	private String mark;
+	private int capacidadBotella;
+	private double precio;
+	private int stock;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public int getCapacidadBotella() {
+		return capacidadBotella;
+	}
+	public void setCapacidadBotella(int capacidadBotella) {
+		this.capacidadBotella = capacidadBotella;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	
 }
