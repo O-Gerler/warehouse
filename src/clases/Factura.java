8168095fcff1743f11c3e8ea0ff11fd3b3cb3a02
Oier <azurmendi5o@outlook.com>
2022-12-24 18:29:24 +0100

    metodo addLinea y pedirNumeroFactura

--- src/clases/Factura.java
+++ src/clases/Factura.java
@@ -1,6 +1,8 @@
 package clases;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Factura {
 	private static final int IVA = 21;
@@ -8,6 +10,7 @@ public class Factura {
 	private String nombreEmpresa;
 	private Date fecha;
 	private String concepto;
+	ArrayList<LineaFactura> lineaFacturas = new ArrayList<>();
 	
 	public int getNumero() {
 		return numero;
@@ -32,5 +35,27 @@ public class Factura {
 	}
 	public void setConcepto(String concepto) {
 		this.concepto = concepto;
+	}
+	
+	public void addLinea(Almacen almacen, Scanner sc) {
+		LineaFactura lineaFactura = new LineaFactura();
+		
+		int numFactura = pedirNumeroFactura(sc);
+		
+	}
+	private int pedirNumeroFactura(Scanner sc) {
+		int numFactura = 0;
+		
+		do {
+			System.out.print("Introduce el numero de factura: ");
+			try {
+				numFactura = Integer.parseInt(sc.nextLine());
+			} catch (Exception e) {
+				System.out.println("ERROR!!!");
+				numFactura = -1;
+			}
+		} while (numFactura < 0);
+		
+		return numFactura;
 	}
 }
