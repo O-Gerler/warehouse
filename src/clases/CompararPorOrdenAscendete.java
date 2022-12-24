9f31f0ee5174fbf7df9f924ee67d97ba55aafe0f
Oier <azurmendi5o@outlook.com>
2022-12-24 16:58:23 +0100

    reescribir metodo compare

--- src/clases/CompararPorOrdenAscendete.java
+++ src/clases/CompararPorOrdenAscendete.java
@@ -1,5 +1,13 @@
 package clases;
 
-public class CompararPorOrdenAscendete {
+import java.util.Comparator;
+
+public class CompararPorOrdenAscendete implements Comparator<Articulo>{
+
+	@Override
+	public int compare(Articulo arg0, Articulo arg1) {
+		// TODO Auto-generated method stub
+		return (int) (arg0.getPrecio() - arg1.getPrecio());
+	}
 
 }
