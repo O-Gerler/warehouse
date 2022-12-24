321a9ab2c678072360a413bac65af4f6e3b89829
Oier <azurmendi5o@outlook.com>
2022-12-24 14:01:24 +0100

    atributos y getters/setters

--- src/clases/Vino.java
+++ src/clases/Vino.java
@@ -3,6 +3,52 @@ package clases;
 import interfaces.Alcoholico;
 
 public class Vino extends Articulo implements Alcoholico{
+	
+	private String color;
+	private String origen;
+	private int anio;
+	private String tipoUva;
+	private double gradosAlcohol;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public String getTipoUva() {
+		return tipoUva;
+	}
+
+	public void setTipoUva(String tipoUva) {
+		this.tipoUva = tipoUva;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
 
 	@Override
 	public boolean esFuerte() {
