2d7a16b73a31e5769cf534ef6f37eb5dce6d498a
Oier <azurmendi5o@outlook.com>
2022-12-24 17:13:46 +0100

    metodo compare

--- src/clases/CompararStockPorOrdenAscendente.java
+++ src/clases/CompararStockPorOrdenAscendente.java
@@ -1,5 +1,12 @@
 package clases;
 
-public class CompararStockPorOrdenAscendente {
+import java.util.Comparator;
+
+public class CompararStockPorOrdenAscendente implements Comparator<Articulo>{
+
+	@Override
+	public int compare(Articulo arg0, Articulo arg1) {
+		return (int)(arg0.getStock() - arg1.getStock());
+	}
 
 }
