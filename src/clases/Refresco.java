8a3fbc46db3a01d83fb6099b58ef9d9a38bf1586
Oier <azurmendi5o@outlook.com>
2022-12-24 13:15:21 +0100

    herencia de clase Articulo y metodos abstractos heredados sobreescritos

--- src/clases/Refresco.java
+++ src/clases/Refresco.java
@@ -1,5 +1,23 @@
 package clases;
 
-public class Refresco {
+public class Refresco extends Articulo{
 
+	@Override
+	public void visualizarAticulo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void precioTotal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void esSaludable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
