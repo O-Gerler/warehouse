9aea46ee9e023204fa36cb9ba91cb8dea4e26eee
Oier <azurmendi5o@outlook.com>
2022-12-24 13:53:55 +0100

    atributos estaticos y emcabezado de metodos

--- src/interfaces/Alcoholico.java
+++ src/interfaces/Alcoholico.java
@@ -1,5 +1,9 @@
 package interfaces;
 
 public interface Alcoholico {
-
+	static final double TASA_BEBIDAS_FUERTES = 64.13;
+	static final double TASA_BEBIDAS_SUAZES = 38.43;
+	
+	public boolean esFuerte();
+	public double calcularTasa();
 }
