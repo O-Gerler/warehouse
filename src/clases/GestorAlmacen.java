3ddcb9b1675461798755fd59c02bde11ff6be706
Oier <azurmendi5o@outlook.com>
2022-12-24 18:48:29 +0100

    metodos traidos desde la clase Factura

--- src/clases/GestorAlmacen.java
+++ src/clases/GestorAlmacen.java
@@ -1,9 +1,55 @@
 package clases;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class GestorAlmacen {
 	public void run() {
 		
 	}
 	
+	private Factura crearFacturaConDatos(Almacen almacen, Scanner sc) {
+		LineaFactura lineaFactura = new LineaFactura();
+		
+		int numFactura = pedirNumero(sc);
+		System.out.print("Introduce el nombre de la empresa: ");
+		String nombreEmpresa = sc.nextLine().trim();
+		Date fecha = introducirFecha(sc);
+		System.out.print("Introduce el concepto de la factura: ");
+		String concepto = sc.nextLine().trim();
+		
+		Factura factura = new Factura(numFactura, nombreEmpresa, fecha, concepto);
+		
+		return factura;
+	}
 	
+	private Date introducirFecha(Scanner sc) {
+		System.out.print("Introduce la fecha(yyyy/MM/dd): ");
+		String fechaIntroducida = sc.nextLine().trim();
+		Date fecha;
+		try {
+			fecha = new SimpleDateFormat("yyyy/MM/dd").parse(fechaIntroducida);
+		} catch (ParseException e) {
+			fecha = new Date();
+		}
+		return fecha;
+	}
+	
+	private int pedirNumero(Scanner sc) {
+		int numFactura = 0;
+		
+		do {
+			System.out.print("Introduce el numero de factura: ");
+			try {
+				numFactura = Integer.parseInt(sc.nextLine());
+			} catch (Exception e) {
+				System.out.println("ERROR!!!");
+				numFactura = -1;
+			}
+		} while (numFactura < 0);
+		
+		return numFactura;
+	}
 }
