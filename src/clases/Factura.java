fb5b3bdf1ac0c4c1ec9a94419711bd098f34249d
Oier <azurmendi5o@outlook.com>
2022-12-24 18:44:15 +0100

    constructor y metodo crearFacturaConDatos

--- src/clases/Factura.java
+++ src/clases/Factura.java
@@ -14,6 +14,16 @@ public class Factura {
 	private String concepto;
 	ArrayList<LineaFactura> lineaFacturas = new ArrayList<>();
 	
+	public Factura() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Factura(int numero, String nombreEmpresa, Date fecha, String concepto) {
+		this.numero = numero;
+		this.nombreEmpresa = nombreEmpresa;
+		this.fecha = fecha;
+		this.concepto = concepto;
+	}
 	public int getNumero() {
 		return numero;
 	}
@@ -39,13 +49,19 @@ public class Factura {
 		this.concepto = concepto;
 	}
 	
-	public void addLinea(Almacen almacen, Scanner sc) {
+	public Factura crearFacturaConDatos(Almacen almacen, Scanner sc) {
 		LineaFactura lineaFactura = new LineaFactura();
 		
 		int numFactura = pedirNumeroFactura(sc);
 		System.out.print("Introduce el nombre de la empresa: ");
 		String nombreEmpresa = sc.nextLine().trim();
 		Date fecha = introducirFecha(sc);
+		System.out.print("Introduce el concepto de la factura: ");
+		String concepto = sc.nextLine().trim();
+		
+		Factura factura = new Factura(numFactura, nombreEmpresa, fecha, concepto);
+		
+		return factura;
 	}
 	private Date introducirFecha(Scanner sc) {
 		System.out.print("Introduce la fecha(yyyy/MM/dd): ");
@@ -58,6 +74,7 @@ public class Factura {
 		}
 		return fecha;
 	}
+	
 	private int pedirNumeroFactura(Scanner sc) {
 		int numFactura = 0;
 		
