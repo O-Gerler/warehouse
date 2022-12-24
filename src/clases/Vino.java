2db223094ba8e1085933cfd729bd147fed632de6
Oier <azurmendi5o@outlook.com>
2022-12-24 13:59:14 +0100

    agregar funciones sin implementar

--- src/clases/Vino.java
+++ src/clases/Vino.java
@@ -1,5 +1,37 @@
 package clases;
 
-public class Vino {
+import interfaces.Alcoholico;
+
+public class Vino extends Articulo implements Alcoholico{
+
+	@Override
+	public boolean esFuerte() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double calcularTasa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void visualizarAticulo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void precioTotal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean esSaludable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
