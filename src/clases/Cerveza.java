153c734aa14ea6057b56b4047eca73bf9505bdda
Oier <azurmendi5o@outlook.com>
2022-12-24 14:28:52 +0100

    metodos sin implementar agregados

--- src/clases/Cerveza.java
+++ src/clases/Cerveza.java
@@ -1,5 +1,37 @@
 package clases;
 
-public class Cerveza {
+import interfaces.Alcoholico;
+
+public class Cerveza extends Articulo implements Alcoholico{
+
+	@Override
+	public boolean esFuerte() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double calcularTasa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void visualizarAticulo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void precioTotal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean esSaludable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
