52d7b1d6692c24f6dae1654a28b7b5f42791ec9b
Oier <azurmendi5o@outlook.com>
2022-12-24 17:27:42 +0100

    atributos setter/getters y constructor

--- src/clases/LineaFactura.java
+++ src/clases/LineaFactura.java
@@ -1,5 +1,34 @@
 package clases;
 
 public class LineaFactura {
-
+	private int numero;
+	private Articulo articulo;
+	private int cantidad;
+	
+	public LineaFactura() {}
+	
+	public LineaFactura(int numero, Articulo articulo, int cantidad) {
+		super();
+		this.numero = numero;
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public Articulo getArticulo() {
+		return articulo;
+	}
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 }
