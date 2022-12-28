1a5b5a99b1b56567dc27537813fc12ef418c5bea
Oier <azurmendi5o@outlook.com>
2022-12-28 21:24:49 +0100

    throws distinto

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,13 @@
 package main;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import clases.GestorAlmacen;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		GestorAlmacen gestorAlmacen = new GestorAlmacen();
 		gestorAlmacen.run();
 	}
